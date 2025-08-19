b2782a9fad76d5a2edad48a3940b2ed3a74d59a4
Vipin Maurya <mevipin2212001@gmail.com>
2025-08-20 00:54:51 +0530

    Backend server change (Render)

--- Frontend/src/utils/Constants.jsx
+++ Frontend/src/utils/Constants.jsx
@@ -1,2 +1,2 @@
 // export const USER_API_ENDPOINT = "http://localhost:3000/api/v1/user"
-export const USER_API_ENDPOINT = "https://manpower-service-supply-rollback.vercel.app/api/v1/user"
+export const USER_API_ENDPOINT = "https://manpower-backend-9jh5.onrender.com/api/v1/user"
