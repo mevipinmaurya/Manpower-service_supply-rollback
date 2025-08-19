f073365fd7e7b113b0a0593f475fdc80092a46fb
Vipin Maurya <mevipin2212001@gmail.com>
2025-08-19 23:23:50 +0530

    API fixed

--- Frontend/src/utils/Constants.jsx
+++ Frontend/src/utils/Constants.jsx
@@ -1,2 +1,2 @@
 // export const USER_API_ENDPOINT = "http://localhost:3000/api/v1/user"
-export const USER_API_ENDPOINT = "https://manpower-service-supply-rollback.vercel.app/user"
+export const USER_API_ENDPOINT = "https://manpower-service-supply-rollback.vercel.app/api/v1/user"
